72e33830bbc56597fe916b20663a07ebc4bbb125
jishnu-acube <jishnu@acubeapp.com>
2025-07-03 18:30:47 +0530

    Add Doctype

---
+++ a3_procurement/a3_procurement/doctype/comparative_statement/comparative_statement.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2025, Acube and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Comparative Statement", {
+	// refresh(frm) {
+
+	// }
+    payment_terms_template: function(frm) {
+        // Triggering the server-side method when payment_terms field is changed
+       frappe.call({
+				method: "erpnext.controllers.accounts_controller.get_payment_terms",
+				args: {
+					terms_template: frm.doc.payment_terms_template,
+					posting_date: frm.doc.due_on    ,
+					grand_total: frm.doc.total,
+					base_grand_total: frm.doc.total,
+				},
+				callback: function(r) {
+					if(r.message && !r.exc) {
+						frm.set_value("payment_schedule", r.message);
+					}
+				}
+			})
+    }
+});
