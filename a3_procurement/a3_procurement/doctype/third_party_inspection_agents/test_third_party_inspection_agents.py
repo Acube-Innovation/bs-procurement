e015cf9c5fe0cbece94768cb59f42eaef57c6860
Ajin-acube <ajin@acubeapp.com>
2025-08-21 12:27:11 +0530

    New changes

---
+++ a3_procurement/a3_procurement/doctype/third_party_inspection_agents/test_third_party_inspection_agents.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Acube and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestThirdPartyInspectionAgents(FrappeTestCase):
+	pass
