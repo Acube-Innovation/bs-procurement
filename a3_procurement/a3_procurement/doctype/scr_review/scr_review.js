18f3287a2a345ba397cc18d1edee059bd9f84993
Ajin-acube <ajin@acubeapp.com>
2025-09-09 13:17:34 +0530

    New changes

--- a3_procurement/a3_procurement/doctype/scr_review/scr_review.js
+++ a3_procurement/a3_procurement/doctype/scr_review/scr_review.js
@@ -6,3 +6,32 @@
 
 // 	},
 // });
+frappe.ui.form.on("SCR Review", {
+    scr_no: function(frm) {
+        if (!frm.doc.scr_no) return;
+
+        // clear old child table
+        frm.clear_table("vendors_proposed_by_project_department");
+
+        frappe.call({
+            method: "frappe.client.get",
+            args: {
+                doctype: "Sub-Contract Request",
+                name: frm.doc.scr_no
+            },
+            callback: function(res) {
+                if (res.message) {
+                    let scr_doc = res.message;
+
+                    // loop through child table table_scrb in Sub-Contract Request
+                    (scr_doc.table_scrb || []).forEach(row => {
+                        let child = frm.add_child("vendors_proposed_by_project_department");
+                        child.vendor = row.supplier; // map supplier → vendor
+                    });
+
+                    frm.refresh_field("vendors_proposed_by_project_department");
+                }
+            }
+        });
+    }
+});
