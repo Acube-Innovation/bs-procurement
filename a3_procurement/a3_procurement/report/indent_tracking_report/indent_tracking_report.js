98bad79cd9d10512353e0a33d167fc9cb2f2db41
Ajin-acube <ajin@acubeapp.com>
2025-10-28 11:53:29 +0530

    new changes

--- a3_procurement/a3_procurement/report/indent_tracking_report/indent_tracking_report.js
+++ a3_procurement/a3_procurement/report/indent_tracking_report/indent_tracking_report.js
@@ -1,6 +1,26 @@
 // Copyright (c) 2025, Acube and contributors
 // For license information, please see license.txt
 
+// frappe.query_reports["Indent Tracking Report"] = {
+//     "filters": [
+//         {
+//             "fieldname": "indent",
+//             "label": __("Indent"),
+//             "fieldtype": "Link",
+//             "options": "Indent",
+//         },
+//         {
+//             "fieldname": "indent_status",
+//             "label": __("Indent Status"),
+//             "fieldtype": "Select",
+//             "options": ["", "Draft", "Submitted", "Cancelled"],
+//         },
+//     ]
+// };
+
+// Copyright (c) 2025, Acube and contributors
+// For license information, please see license.txt
+
 frappe.query_reports["Indent Tracking Report"] = {
     "filters": [
         {
@@ -14,6 +34,18 @@ frappe.query_reports["Indent Tracking Report"] = {
             "label": __("Indent Status"),
             "fieldtype": "Select",
             "options": ["", "Draft", "Submitted", "Cancelled"],
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
         },
     ]
 };
